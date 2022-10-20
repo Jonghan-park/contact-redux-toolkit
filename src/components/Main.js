bdcb073157f959de4417bc846ea703d9d46ec97e
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-20 13:46:48 -0600

    Update add button and modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,23 @@ import React from "react";
 import Contact from "./Contact";
 import { useSelector, useDispatch } from "react-redux";
 import { clearContact } from "../features/contact/contactSlice";
+import { openModal } from "../features/modal/modalSlice";
+import AddContactModal from "./AddContactModal";
 
 const Main = () => {
   const dispatch = useDispatch();
   const { contacts, total } = useSelector((state) => state.contact);
+  const { isOpen } = useSelector((state) => state.modal);
   return (
     <>
+      {isOpen && (
+        <div className="add_contact_container">
+          <AddContactModal />
+        </div>
+      )}
+      <div className="add_button">
+        <button onClick={() => dispatch(openModal())}>Add Contact</button>
+      </div>
       <div className="main_container">
         <Contact contacts={contacts} />
       </div>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactReducer from "./features/contact/contactSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     contact: contactReducer,
+    modal: modalReducer,
   },
 });
